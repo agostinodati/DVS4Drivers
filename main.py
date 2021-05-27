a48409d2bcfd993336e5aeea744a1500580c2c07
ABTCoder <amalbenson98@gmail.com>
2021-05-27 22:40:45 +0200

    Test reading aedat4 file

--- main.py
+++ main.py
@@ -1,10 +1,31 @@
+from dv import NetworkEventInput
+from dv import AedatFile
+import cv2
+
 
 def main():
-    print("Test")
+    with AedatFile("C:/Users/User/Downloads/dvSave-2021_04_23_13_45_03.aedat4") as f:
+        # list all the names of streams in the file
+        print(f.names)
+
+        # Access dimensions of the event stream
+        height, width = f['events'].size
+
+        '''# loop through the "events" stream
+        for e in f['events']:
+            print(e.timestamp)'''
+
+        # loop through the "events" stream as numpy packets
+        for e in f['events'].numpy():
+            print(e.shape)
+
+        # loop through the "frames" stream
+        for frame in f['frames']:
+            print(frame.timestamp)
+            cv2.imshow('out', frame.image)
+            cv2.waitKey(1)
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     main()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
