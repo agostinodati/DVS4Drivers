Some other tests and small fix in the normalization

main.py
@@ -36,10 +36,12 @@ def main():
 
         normalize = True  # For normalization relative to timestamps
         k = 0
-        s = 0
+        s = 1
         time = 1000  # 100 fps
         ts = events[0]['timestamp']
         event_frame = np.zeros((height, width, 3), np.uint8)
+        # event_frame = np.zeros((height, width, 1), np.uint8)
+        # event_frame[:,:,0] = 127
         while k != len(events):
 
             # 1 millisecond skip for each frame (100 fps video)
@@ -48,19 +50,22 @@ def main():
                 e = events[k]
                 k += 1
                 if normalize:
-                    norm_factor = (e['timestamp']-ts+s*time) / time
+                    norm_factor = (ts+s*time - e['timestamp']) / time
                 else:
                     norm_factor = 1
 
                 if e['polarity'] == 1:
                     event_frame[e['y'], e['x']] = (0, int(255*norm_factor), 0)
+                    # event_frame[e['y'], e['x']] = int(127 * norm_factor) + 127
                 else:
-                    event_frame[e['y'], e['x']] = (0, 0, int(255*norm_factor))
+                    event_frame[e['y'], e['x']] = (int(255*norm_factor), 0, 0)
+                    # event_frame[e['y'], e['x']] = 127-int(127 * norm_factor)
 
             s += 1
             cv2.imshow('out3', event_frame)
             # Frame reset
             event_frame = np.zeros((height, width, 3), np.uint8)
+            # event_frame[:, :, :] = 0
             cv2.waitKey(1)
 
         print(k)