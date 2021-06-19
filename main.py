5d348239e1d00b679c9c362789e3ae7dcac1e0b2
agostinodati <agostino.dati@outlook.it>
2021-06-19 16:02:06 +0200

    Parametrized some function

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from dv import AedatFile
 import cv2
 import numpy as np
 import mediapipe as mp
+import os.path
 
 
 def main1():
@@ -116,25 +117,25 @@ def main2():
 
 
 def main3():
-    with AedatFile("D:/Download/dvSave-2021_04_23_13_45_03.aedat4") as f:
+    # D:/Download/dvSave-2021_04_23_13_45_03.aedat4
+    print("Welcome!")
+    filepath = input('*.Aedat4 file path: ')
+    while not os.path.isfile(filepath):
+        print('File does not exists, try again.')
+        filepath = input('*.Aedat4 file path: ')
+
+    with AedatFile(filepath) as f:
         # list all the names of streams in the file
         print(f.names)
 
         # Access dimensions of the event stream
         height, width = f['events'].size
 
-        i = 0
-        for frame in f['frames']:
-            # cv2.imshow('out', frame.image)
-            cv2.imwrite("D:/openCv/test.jpg", frame.image)
-            i += 1
-            #  cv2.waitKey(3)
-            break
-        print(i)
+        # find_landmarks_static(f['frames'])
 
-        video = frame2avi(f['frames'])
+        path_video = frame2avi(f['frames'])
 
-        find_landmarks_frames(video)
+        find_landmarks_frames(path_video)
 
         normalize = False  # For normalization relative to timestamps
         k = 0  # Event counter
@@ -173,31 +174,42 @@ def main3():
         print(s)
 
 
-def frame2avi(frames):
-    codec = 0
-    fps = 25
-    height, width = frames.size
-    size = (width, height)
-    path = "D:/openCv/videoTest.avi"
-    out = cv2.VideoWriter(path, codec, fps, size)
-    i = 0
-    print('Creating the *.avi...')
+def check_dir(path):
+    if not os.path.exists(path):
+        print('Directory ' + '"if os.path.exists(path):"' + ' does not exists. /nCreating the dir...')
+        os.makedirs(path)
+    if not os.path.isdir(path):
+        print('The path indicated is not a directory.')
+        return False
+    return True
+
+
+def find_landmarks_static(frames):
+    '''
+        :param frames: List of Frames of the Aedat4 file.
+
+        This function take the first frame of the aedat4 file and save it as image. Then, find the face landmarks.
+    '''
+    print('You are in the Static mode.')
+
+    path = input('Path where to save the img of the video: ')
+    while not check_dir(path):
+        path = input('Path where to save the img of the video: ')
+
+    path_results = input('Path where to save the img of the results: ')
+    while not check_dir(path):
+        path_results = input('Path where to save the img of the results: ')
+
+    save_path = path + '/frame.jpg'
+
     for frame in frames:
-        cv2.imwrite('D:/openCv/frames/frame_' + str(i) + '.jpg', frame.image)
-        img = cv2.imread("D:/openCv/frames/frame_" + str(i) + ".jpg")
-        out.write(img)
-        i = i + 1
-        print('...')
-    cv2.destroyAllWindows()
-    out.release()
-    print('*.avi created!')
-    return path
+        cv2.imwrite(save_path, frame.image)
+        break
 
-
-def find_landmarks_static():
     mp_drawing = mp.solutions.drawing_utils
     mp_face_mesh = mp.solutions.face_mesh
-    image_files = ["D:/openCv/test.jpg"]
+    image_files = [save_path]
+
     drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
     with mp_face_mesh.FaceMesh(
             static_image_mode=True,
@@ -220,16 +232,61 @@ def find_landmarks_static():
                     connections=mp_face_mesh.FACE_CONNECTIONS,
                     landmark_drawing_spec=drawing_spec,
                     connection_drawing_spec=drawing_spec)
-            cv2.imwrite('D:/openCv/annotated_image' + str(idx) + '.png', annotated_image)
+            cv2.imwrite(path_results + '/annotated_image' + str(idx) + '.png', annotated_image)
             cv2.imshow('out', annotated_image)
             cv2.waitKey(1)
 
 
-def find_landmarks_frames(video):
+def frame2avi(frames):
+    '''
+        :param frames: List of Frames of the Aedat4 file.
+        :return: Path of the created video.
+
+        This function create a video (*.avi) from the Frames of the Aedat4 file.
+    '''
+    # D:/openCV
+    path = input('Path where to save the *.avi: ')
+    while not check_dir(path):
+        path = input('Path where to save the *.avi: ')
+    path_frames = path + '/frames'
+    name = '/videoTest.avi'
+    path_video = path + name
+
+    if os.path.isfile(path_video):
+        print('The file' + '"' + path_video + '"' + ' already exists.')
+        return path_video
+
+    codec = 0
+    fps = 25
+    height, width = frames.size
+    size = (width, height)
+    out = cv2.VideoWriter(path_video, codec, fps, size)
+    i = 0
+    print('Creating the *.avi...')
+    for frame in frames:
+        cv2.imwrite(path_frames + '/frame_' + str(i) + '.jpg', frame.image)
+        img = cv2.imread(path_frames + '/frame_' + str(i) + '.jpg')
+        out.write(img)
+        i = i + 1
+        print('...')
+    cv2.destroyAllWindows()
+    out.release()
+    print('*.avi created!')
+    return path_video
+
+
+def find_landmarks_frames(path_video):
+    '''
+        :param path_video: Path of the *.avi file.
+
+        This function finds face's landmarks.
+    '''
     mp_drawing = mp.solutions.drawing_utils
     mp_face_mesh = mp.solutions.face_mesh
+
     drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
-    cap = cv2.VideoCapture(video)
+    cap = cv2.VideoCapture(path_video)
+
     with mp_face_mesh.FaceMesh(
             min_detection_confidence=0.5,
             min_tracking_confidence=0.5) as face_mesh:
