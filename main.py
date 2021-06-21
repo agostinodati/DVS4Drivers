eca409f5db33553897d6936561cb11d09ecd2ce4
agostinodati <agostino.dati@outlook.it>
2021-06-21 18:49:46 +0200

    Impelented a draw function for specific facemesh landmarks

--- main.py
+++ main.py
@@ -6,6 +6,31 @@ import mediapipe as mp
 import os.path
 import utility
 
+lipsUpperOuter = [61, 185, 40, 39, 37, 0, 267, 269, 270, 409, 291]
+lipsLowerOuter = [146, 91, 181, 84, 17, 314, 405, 321, 375, 291]
+lipsUpperInner = [78, 191, 80, 81, 82, 13, 312, 311, 310, 415, 308]
+lipsLowerInner = [78, 95, 88, 178, 87, 14, 317, 402, 318, 324, 308]
+
+rightEyeUpper0 = [246, 161, 160, 159, 158, 157, 173]
+rightEyeLower0 = [33, 7, 163, 144, 145, 153, 154, 155, 133]
+rightEyeUpper1 = [247, 30, 29, 27, 28, 56, 190]
+rightEyeLower1 = [130, 25, 110, 24, 23, 22, 26, 112, 243]
+rightEyeUpper2 = [113, 225, 224, 223, 222, 221, 189]
+rightEyeLower2 = [226, 31, 228, 229, 230, 231, 232, 233, 244]
+rightEyeLower3 = [143, 111, 117, 118, 119, 120, 121, 128, 245]
+
+leftEyeUpper0 = [466, 388, 387, 386, 385, 384, 398]
+leftEyeLower0 = [263, 249, 390, 373, 374, 380, 381, 382, 362]
+leftEyeUpper1 = [467, 260, 259, 257, 258, 286, 414]
+leftEyeLower1 = [359, 255, 339, 254, 253, 252, 256, 341, 463]
+leftEyeUpper2 = [342, 445, 444, 443, 442, 441, 413]
+leftEyeLower2 = [446, 261, 448, 449, 450, 451, 452, 453, 464]
+leftEyeLower3 = [372, 340, 346, 347, 348, 349, 350, 357, 465]
+
+zero_landmark = rightEyeUpper1 + rightEyeLower1 + leftEyeUpper1 + leftEyeLower1
+
+all_landmark = lipsUpperInner + lipsLowerInner + zero_landmark
+
 
 def main1():
     with AedatFile("D:/Utorrent/dvSave-2021_05_28_18_48_58.aedat4") as f:
@@ -87,6 +112,7 @@ def main2():
         time = 16000  # for 100 fps -> 1000 us
         event_frame = np.zeros((height, width, 3), np.uint8)
         video_frame = f['frames'].__next__()
+        annotated_image = find_landmarks_frame(video_frame)
         for packet in f['events'].numpy():
             for e in packet:
 
@@ -115,12 +141,12 @@ def main2():
                 # 1 millisecond skip for each frame (100 fps video)
                 # All events in this time window are combined into one frame
                 if e['timestamp'] > ts + s * time:
-                    cv2.imshow('out3', event_frame)
-                    annotated_image = find_landmarks_frame(video_frame)
-                    cv2.imshow('out2', video_frame.image)
-                    cv2.imshow('out1', annotated_image)
+                    cv2.imshow('Events', event_frame)
+                    cv2.imshow('Video', video_frame.image)
+                    cv2.imshow('Facemesh', annotated_image)
                     if video_frame.timestamp < ts + s * time:
                         video_frame = f['frames'].__next__()
+                        annotated_image = find_landmarks_frame(video_frame)
                     s += 1
 
                     # Frame reset
@@ -194,6 +220,8 @@ def find_landmarks_frame(frame):
 
     drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
 
+    height, width = frame.size
+
     with mp_face_mesh.FaceMesh(
             min_detection_confidence=0.5,
             min_tracking_confidence=0.5) as face_mesh:
@@ -212,13 +240,18 @@ def find_landmarks_frame(frame):
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
         if results.multi_face_landmarks:
             for face_landmarks in results.multi_face_landmarks:
-                mp_drawing.draw_landmarks(
+                '''mp_drawing.draw_landmarks(
                     image=image,
                     landmark_list=face_landmarks,
                     connections=mp_face_mesh.FACE_CONNECTIONS,
                     landmark_drawing_spec=drawing_spec,
-                    connection_drawing_spec=drawing_spec)
-        return image
+                    connection_drawing_spec=drawing_spec)'''
+                # rightEyeUpper0: [246, 161, 160, 159, 158, 157, 173],
+                for index in all_landmark:
+                    image2 = cv2.circle(image, (
+                    int(face_landmarks.landmark[index].x * height), int(face_landmarks.landmark[index].y * width)),
+                                        radius=0, color=(0, 0, 255), thickness=-1)
+        return image2
 
 
 def find_landmarks_frames(frames):
